bedda3e6c921f204a409a13cd14d247c60eb2629
Alejandro Armas <lxarmas@gmail.com>
2024-05-07 23:11:13 +0200

    break_sleep

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,15 @@ import MainLayOut from './layouts/MainLayOut';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage, {jobLoader} from './pages/JobPage';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayOut/>}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage />} />
   </Route>
   )
