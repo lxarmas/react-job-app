c22a8baea15d82abe324c94cac5771b0db3fdbd7
Alejandro Armas <lxarmas@gmail.com>
2024-05-08 23:45:39 +0200

    delete_JobPage

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import AddJobPage from './pages/AddJobPage';
 
 
 const App = () => {
+  // add a new job
   const addJob = async ( newJob ) => {
     const res = await fetch( '/api/jobs', {
       method: 'POST',
@@ -25,7 +26,13 @@ const App = () => {
     } );
     return;
   };
-
+  // delete job 
+  const deleteJob = async ( id )=> {
+  const res = await fetch( `/api/jobs/${id}`, {
+    method: 'DELETE',
+  } );
+    return;
+  } 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,7 +40,9 @@ const router = createBrowserRouter(
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
       <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+
+      <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} /
+>} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage />} />
   </Route>
   )
--- src/layouts/MainLayOut.jsx
+++ src/layouts/MainLayOut.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '../components/Navbar';
 
 const MainLayOut = () => {
   return (
       <>
-        <Navbar/>  
-        <Outlet/>  
+      <Navbar/>  
+      <Outlet /> 
+      <ToastContainer/>
       </>
   )
 }
